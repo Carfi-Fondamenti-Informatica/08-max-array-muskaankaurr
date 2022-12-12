9a4417af86a173b8f17ee7e43301af4968459a29
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-12 18:07:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+float ValoreMaggiore ( float numeri[], int a){
+for(int i=0; i<a; ++i) {
+    if ( numeri[0] > numeri[i]){
 
+    }else{
+        numeri[0] = numeri[i];
+    }
+}
+return numeri [0];
+}
