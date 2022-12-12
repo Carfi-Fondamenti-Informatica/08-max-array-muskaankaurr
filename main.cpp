1c7db555671cf7f74169cc78bc86c79c218ad7ea
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-12 18:04:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+   int a=0;
+   cin >> a;
+   float numeri[a];
+   for(int i=0; i<a ;i++){
+       cin >> numeri [i];
+   }
+cout << ValoreMaggiore (numeri, a) << endl;
+
+    return 0;
 }
